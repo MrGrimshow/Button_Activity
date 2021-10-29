331fae2e761da6c24846ff145836880609e69400
Herbert <grimshowmaino@gmil.com>
2021-10-29 17:18:30 +0000

    Initial commit

---
+++ app/src/main/java/com/example/buttonactivity/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.buttonactivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.buttonactivity.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ClickMe(View view){
+        Intent intent=new Intent( packageContex: MainActivity.this,Activity2.class);
+
+        startActivity(intent);
+    }
+}
